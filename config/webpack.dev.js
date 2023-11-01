b97c33dcce3c4064a66a41ac622e1bd91ad785e2
123456hawkins <2398430768@qq.com>
2023-11-01 14:48:54 +0800

    新增生产模式和开发模式

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,26 +1,20 @@
-// Nodejs的核心模块,专门用来处理文件路径
 const path = require('path')
 const ESLintWebpackPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  // 入口
   entry: './src/main.js',
-  // 输出
   output: {
-    // path: 文件输出目录，必须是绝对路径
-    // path.resolve()方法返回一个绝对路径
-    // __dirname 当前文件的文件夹绝对路径
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'static/js/main.js', //将js文件输出到static/js目录中
-    clean: true, //自动清空上次打包资源
+    path: undefined, // 开发模式没有输出，不需要指定输出目录
+    filename: 'static/js/main.js', // 将 js 文件输出到 static/js 目录中
+    // clean: true, // 开发模式没有输出，不需要清空输出结果
   },
-  // 加载器
   module: {
     rules: [
       {
-        // 用来匹配.css结尾的文件
+        // 用来匹配 .css 结尾的文件
         test: /\.css$/,
-        // use数组里面Loader执行顺序从左往右
+        // use 数组里面 Loader 执行顺序是从右到左
         use: ['style-loader', 'css-loader'],
       },
       {
@@ -28,18 +22,21 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'less-loader'],
       },
       {
-        test: /\.s[sc]ss$/,
+        test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|webp|svg)$/,
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
         type: 'asset',
         parser: {
           dataUrlCondition: {
-            maxSize: 10 * 1024, //小于10kb的图表会被base64处理
+            maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
           },
         },
-        // 修改输出资源的名称和路径
         generator: {
           // 将图片文件输出到 static/imgs 目录中
           // 将图片文件命名 [hash:8][ext][query]
@@ -50,7 +47,6 @@ module.exports = {
         },
       },
       {
-        // 处理字体资源
         test: /\.(ttf|woff2?)$/,
         type: 'asset/resource',
         generator: {
@@ -58,33 +54,31 @@ module.exports = {
         },
       },
       {
-        // 处理音视频资源
-        test: /\.(ttf|woff2?|map4|map3|avi)$/,
+        test: /\.js$/,
+        exclude: /node_modules/, // 排除node_modules代码不编译
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.mp3$/,
         type: 'asset/resource',
         generator: {
           filename: 'static/media/[hash:8][ext][query]',
         },
       },
-      {
-        // babel
-        test: /\.js$/,
-        exclude: /node_modules/, // 排除node_modules代码不编译
-        loader: 'babel-loader',
-      },
     ],
   },
-  // 插件
   plugins: [
     new ESLintWebpackPlugin({
-      context: path.resolve(__dirname, 'src'),
+      // 指定检查文件的根目录
+      context: path.resolve(__dirname, '../src'),
     }),
     new HtmlWebpackPlugin({
-      // 以public/index.html为模板创建文件
-      // 新的html文件有两个特点:1.内容和源文件一致 2.自动引入打包生成的js等资源
-      template: path.resolve(__dirname, 'public/index.html'),
+      // 以 public/index.html 为模板创建文件
+      // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+      template: path.resolve(__dirname, '../public/index.html'),
     }),
   ],
-  // 开发服务器
+  // 其他省略
   devServer: {
     host: 'localhost', // 启动服务器域名
     port: '3000', // 启动服务器端口号
